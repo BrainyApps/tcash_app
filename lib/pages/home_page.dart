ade161afc8edbfcce0d1296884fe6f7cf40cb362
ammshahin <ammshahin@gmail.com>
2023-05-12 17:36:21 +0600

    payment page boilarplate added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:rnd_flutter_app/pages/payment_page.dart';
 import 'package:rnd_flutter_app/pages/transaction_history.dart';
 import 'package:rnd_flutter_app/provider/login_provider.dart';
 
@@ -57,11 +58,11 @@ class _HomePageState extends State<HomePage> {
     return Container(
         padding: const EdgeInsets.all(10.0),
         child: Scaffold(
-          body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Container(
+            body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Container(
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
@@ -74,121 +75,125 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ],
                 ),
-                  margin: const EdgeInsets.only(bottom: 20),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(
-                            left: 20.0, top: 8.0, bottom: 8.0),
-                        alignment: Alignment.centerLeft,
-                        child: const Text(
-                          'Current Balance',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(255, 59, 163, 243),
-                          ),
+                margin: const EdgeInsets.only(bottom: 20),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(
+                          left: 20.0, top: 8.0, bottom: 8.0),
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Current Balance',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 59, 163, 243),
                         ),
                       ),
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        alignment: Alignment.centerLeft,
-                        child: Row(children: const [
-                          Expanded(
-                              child: Text(
-                            '৳ 100.00',
-                            style: TextStyle(
-                                fontSize: 30, fontWeight: FontWeight.bold),
-                          )),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: CircleAvatar(
-                              backgroundColor: Colors.blue,
-                              radius: 20,
-                              child: Icon(
-                                Icons.add,
-                                color: Colors.white,
-                                size: 30,
-                              ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      alignment: Alignment.centerLeft,
+                      child: Row(children: const [
+                        Expanded(
+                            child: Text(
+                          '৳ 100.00',
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        )),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.blue,
+                            radius: 20,
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 30,
                             ),
-                          )
-                        ]),
+                          ),
+                        )
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 4,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 5,
+                children: [
+                  GridItem(
+                      key: UniqueKey(), title: 'Send Money', icon: Icons.send),
+                  GridItem(
+                      key: UniqueKey(),
+                      title: 'Add Money',
+                      icon: Icons.attach_money),
+                  GridItem(
+                      key: UniqueKey(),
+                      title: 'Cash Out',
+                      icon: Icons.monetization_on),
+                  GridItem(
+                      key: UniqueKey(), title: 'Payment', icon: Icons.payment),
+                ],
+              ),
+              Container(
+                  margin: const EdgeInsets.only(top: 15),
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 2,
+                        offset:
+                            const Offset(0, 1), // changes position of shadow
                       ),
                     ],
                   ),
-                ),
-                GridView.count(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  crossAxisCount: 4,
-                  mainAxisSpacing: 5,
-                  crossAxisSpacing: 5,
-                  children: [
-                    GridItem(
-                        key: UniqueKey(),
-                        title: 'Send Money',
-                        icon: Icons.send),
-                    GridItem(
-                        key: UniqueKey(),
-                        title: 'Add Money',
-                        icon: Icons.attach_money),
-                    GridItem(
-                        key: UniqueKey(),
-                        title: 'Cash Out',
-                        icon: Icons.monetization_on),
-                    GridItem(
-                        key: UniqueKey(),
-                        title: 'Payment',
-                        icon: Icons.payment),
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 15),
-                    padding: const EdgeInsets.all(20),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.1),
-                          spreadRadius: 1,
-                          blurRadius: 2,
-                          offset:
-                              const Offset(0, 1), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween, 
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Text(
-                              'Financial Management',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                              ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Financial Management',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
                             ),
-                            SizedBox(height: 8),
-                            Text(
-                              'Your expense today ৳ 24',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 16,
-                              ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Your expense today ৳ 24',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
                             ),
-                          ],
-                        ),
-                        Column(
+                          ),
+                        ],
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Payment()));
+                        },
+                        child: Column(
                           children: const [
                             Icon(Icons.arrow_forward_ios),
                           ],
-                        )
-                      ],
+                        ),
+                      )
+                    ],
                   )),
               Container(
                 height: 215,
@@ -213,7 +218,6 @@ class _HomePageState extends State<HomePage> {
               )
             ],
           ),
-        ) 
-        ));
+        )));
   }
 }
